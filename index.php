f271eed792916c1d89a0921b0842aa60450d4b01
Mardani JH <dani_4uvi@yahoo.com>
2021-01-08 08:42:07 +0800

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote dari Local</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolores dolor repellendus corporis voluptatem, tenetur dolorum. Temporibus recusandae est harum officia, reiciendis, fugit sunt sint earum praesentium, adipisci tenetur doloribus.</p>
+</body>
+</html>
